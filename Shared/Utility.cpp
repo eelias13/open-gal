e8cf616f0007591aa5714c714cc2842698736230
eeli1 <maierelias13@gmail.com>
2021-06-03 11:43:35 +0200

    ups forgot to save that

--- Shared/Utility.cpp
+++ Shared/Utility.cpp
@@ -9,16 +9,16 @@
 
 void showHelpMenu()
 {
-    cout << "usage of EasyGAL: " << endl;
-    cout << '\t' << "EasyGAL [EasyGAl code] [name of output jedec] [gal type]" << endl;
-    cout << '\t' << "EasyGAL api [table data json] [name of output jedec] [gal type]" << endl;
-    cout << '\t' << "EasyGAL api [EasyGAl code] [table data json] [gal type]" << endl;
-    cout << '\t' << "EasyGAL api [EasyGAl code] [table data json]" << endl;
+    cout << "usage of OpenGal: " << endl;
+    cout << '\t' << "OpenGal [OpenGal code] [name of output jedec] [gal type]" << endl;
+    cout << '\t' << "OpenGal api [table data json] [name of output jedec] [gal type]" << endl;
+    cout << '\t' << "OpenGal api [OpenGal code] [table data json] [gal type]" << endl;
+    cout << '\t' << "OpenGal api [OpenGal code] [table data json]" << endl;
     cout << "Exampel:" << endl;
-    cout << '\t' << "EasyGAL code.txt out.jedec g22v10" << endl;
-    cout << '\t' << "EasyGAL api tableData.json out.jedec g22v10" << endl;
-    cout << '\t' << "EasyGAL api code.txt tableData.json g22v10" << endl;
-    cout << '\t' << "EasyGAL api code.txt tableData.json" << endl;
+    cout << '\t' << "OpenGal code.txt out.jedec g22v10" << endl;
+    cout << '\t' << "OpenGal api tableData.json out.jedec g22v10" << endl;
+    cout << '\t' << "OpenGal api code.txt tableData.json g22v10" << endl;
+    cout << '\t' << "OpenGal api code.txt tableData.json" << endl;
 }
 
 void initDeviceType(Configs::CircuitConfig &DeviceType, string deviceName, vector<uint32_t> &inputPins, vector<uint32_t> &outputPins)
