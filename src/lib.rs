c43c5888a0ca958201a98e2f9263dd66cd727f34
elias <maierelias13@gmail.com>
2021-10-14 13:05:16 +0200

    separating code into parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
-mod atom;
-mod atomizer;
 mod constants;
-mod error;
-mod lexer;
 mod parser;
-mod table_parser;
-mod token;
 mod utils;
 mod table_data;
 
---
+++ src/parser/mod.rs
@@ -0,0 +1,7 @@
+pub mod atom;
+pub mod atomizer;
+pub mod error;
+pub mod lexer;
+pub mod table_parser;
+pub mod token;
+pub mod parser;
