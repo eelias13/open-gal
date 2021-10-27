fb1cfcb84ea87433a1200927103b8bfc373ac4ec
elias <maierelias13@gmail.com>
2021-10-27 14:02:56 +0200

    removing verilog

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod parser;
 mod table_data;
 
 pub use table_data::TableData;
+
 pub fn parse(data: Vec<&str>) -> Result<Vec<TableData>, String> {
     match parser::core::parse(data) {
         Err(parsing_error) => Err(format!("{}", parsing_error)),
