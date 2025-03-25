b2299c687905fef7bf3430b799384c8d6c5dab79
eelias13 <maierelias13@gmai.com>
2025-03-25 17:46:17 +0100

    change editor

--- editor/build.sh
+++ editor/build.sh
@@ -1,9 +1,6 @@
-cd rust && wasm-pack build --target no-modules --no-typescript --release && cd ..
-mkdir build
-cp -r vs build/vs
+cd rust 
+wasm-pack build --target no-modules --no-typescript --release  
+cd ..
 
-minifier -o build/script.js web/script.js
-minifier -o build/rust.js rust/pkg/rust.js
-
-cp web/index.html build/index.html
-cp rust/pkg/rust_bg.wasm build/rust_bg.wasm
+cp rust/pkg/rust_bg.wasm web/rust_bg.wasm
+cp rust/pkg/rust.js web/wasm_loader.js
--- editor/web/script.js
+++ editor/web/script.js
@@ -1,168 +1,202 @@
-let code = getOpenGal();
-let theme = "vs-dark";
-const { rs_compile, rs_transpile, rs_tabledata } = wasm_bindgen;
-let editor;
-let galtype = "g22v10";
-let filename = "out";
+let galType = "g22v10";
 
-function setLanguages(monaco) {
-  monaco.languages.register({
-    id: "open-gal",
-  });
 
-  monaco.languages.setMonarchTokensProvider("open-gal", {
-    keywords: ["fill", "count", "dff"],
-    typeKeywords: ["pin", "table"],
-    operators: ["=", "!", "&", "|", "^", "->"],
-    symbols: /[=><!~?:&|+\-*\/\^%]+/,
-    escapes:
-      /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-    tokenizer: {
-      root: [
-        [
-          /[a-z_$][\w$]*/,
-          {
-            cases: {
-              "@typeKeywords": "keyword",
-              "@keywords": "keyword",
-              "@default": "identifier",
-            },
-          },
-        ],
-        [/[A-Z][\w\$]*/, "type.identifier"],
-        { include: "@whitespace" },
-        [/[{}()\[\]]/, "@brackets"],
-        [/[<>](?!@symbols)/, "@brackets"],
-        [/@symbols/, { cases: { "@operators": "operator", "@default": "" } }],
-        [/\d+/, "number"],
-        [/[;,.]/, "delimiter"],
-      ],
-      comment: [
-        [/[^\/*]+/, "comment"],
-        [/\/\*/, "comment", "@push"],
-        ["\\*/", "comment", "@pop"],
-        [/[\/*]/, "comment"],
-      ],
-      whitespace: [
-        [/[ \t\r\n]+/, "white"],
-        [/\/\*/, "comment", "@comment"],
-        [/\/\/.*$/, "comment"],
-      ],
-    },
-  });
+
+
+function get_title() {
+    return "OpenGal to JEDEC Compiler"
 }
 
-function setUpEditor(container) {
-  getCode();
-  require.config({ paths: { vs: "vs/" } });
-  require(["vs/editor/editor.main"], function () {
-    setLanguages(monaco);
-    editor = monaco.editor.create(container, {
-      theme: theme,
-      value: code,
-      language: "open-gal",
+function setup_editor(monaco, editor_el) {
+    monaco.languages.register({
+        id: "open-gal",
     });
-  });
+
+    monaco.languages.setMonarchTokensProvider("open-gal", {
+        keywords: ["fill", "count", "dff"],
+        typeKeywords: ["pin", "table"],
+        operators: ["=", "!", "&", "|", "^", "->"],
+        symbols: /[=><!~?:&|+\-*\/\^%]+/,
+        escapes:
+            /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+        tokenizer: {
+            root: [
+                [
+                    /[a-z_$][\w$]*/,
+                    {
+                        cases: {
+                            "@typeKeywords": "keyword",
+                            "@keywords": "keyword",
+                            "@default": "identifier",
+                        },
+                    },
+                ],
+                [/[A-Z][\w\$]*/, "type.identifier"],
+                { include: "@whitespace" },
+                [/[{}()\[\]]/, "@brackets"],
+                [/[<>](?!@symbols)/, "@brackets"],
+                [/@symbols/, { cases: { "@operators": "operator", "@default": "" } }],
+                [/\d+/, "number"],
+                [/[;,.]/, "delimiter"],
+            ],
+            comment: [
+                [/[^\/*]+/, "comment"],
+                [/\/\*/, "comment", "@push"],
+                ["\\*/", "comment", "@pop"],
+                [/[\/*]/, "comment"],
+            ],
+            whitespace: [
+                [/[ \t\r\n]+/, "white"],
+                [/\/\*/, "comment", "@comment"],
+                [/\/\/.*$/, "comment"],
+            ],
+        },
+    });
+
+    const editor = monaco.editor.create(editor_el, {
+        value: getCode(),
+        language: 'open-gal',
+        theme: 'vs-dark'
+    });
+
+    return editor;
 }
 
-function getOpenGal() {
-  return [
-    "pin 1, 2 = i[0..1];",
-    "pin [14..17] = and, or, xor, not;",
-    "",
-    "table(i0, i1 -> and).fill(0) {",
-    "    11 1",
-    "}",
-    "",
-    "table(i0, i1 -> or).fill(1) {",
-    "    00 0",
-    "}",
-    "",
-    "table(i0, i1 -> xor).count {",
-    "    0",
-    "    1",
-    "    1",
-    "    0",
-    "}",
-    "",
-    "table(i0 -> not) {",
-    "    01",
-    "    10",
-    "}",
-    "",
-    "",
-    "pin 23 = a;",
-    "pin 3 = b;",
-    "pin 2 = c;",
-    "",
-    "a = (!b | (c));",
-    "a.dff;",
-  ].join("\n");
+
+function run_code(editor, output_el) {
+
+    const code = editor.getValue();
+    const result = wasm_bindgen.rs_compile(code, galType);
+    console.log(result);
+
+    if (result.Err != null) {
+        output_el.innerText += result.Err;
+    } else {
+        download(result.Ok, "open_gal.jed");
+        output_el.innerText += "Compiled successfully";
+    }
+
 }
 
-function download(filename, text) {
-  var element = document.createElement("a");
-  element.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-  );
-  element.setAttribute("download", filename);
-  element.style.display = "none";
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+
+function setup_cmd(editor, output_el) {
+    const commands = {
+        // add commands here
+        help: () => "Available commands: help, clear ...",
+        compile: (args) => {
+            const code = editor.getValue();
+            const result = wasm_bindgen.rs_compile(code, galType);
+
+            if (args.length === 0) {
+                if (result.Err != null) {
+                    output_el.innerText += result.Err;
+                } else {
+                    output_el.innerText += "Compiled successfully\n";
+                    output_el.innerText += result.Ok;
+                }
+            } else if (args.length === 2 && args[0] == ">") {
+                if (result.Err != null) {
+                    output_el.innerText += result.Err;
+                } else {
+                    download(result.Ok, args[1]);
+                    output_el.innerText += "Compiled successfully";
+                }
+            } else {
+                output_el.innerText += "ERROR: unrecognized arguments for `compile`"
+            }
+
+        },
+        transpile: (args) => {
+            const code = editor.getValue();
+            const result = wasm_bindgen.rs_transpile(code);
+
+            if (args.length === 0) {
+                if (result.Err != null) {
+                    output_el.innerText += result.Err;
+                } else {
+                    output_el.innerText += "Transpiled successfully\nWin Cuple code:\n";
+                    output_el.innerText += result.Ok;
+                }
+            } else if (args.length === 2 && args[0] == ">") {
+                if (result.Err != null) {
+                    output_el.innerText += result.Err;
+                } else {
+                    download(result.Ok, args[1]);
+                    output_el.innerText += "Transpiled successfully";
+                }
+            } else {
+                output_el.innerText += "ERROR: unrecognized arguments for `transpile`"
+            }
+        },
+        tabledata: (args) => {
+            const code = editor.getValue();
+            const result = wasm_bindgen.rs_tabledata(code);
+
+            if (args.length === 0) {
+                if (result.Err != null) {
+                    output_el.innerText += result.Err;
+                } else {
+                    output_el.innerText += JSON.stringify(result.Ok, null, 2);
+                }
+            } else if (args.length === 2 && args[0] == ">") {
+                if (result.Err != null) {
+                    output_el.innerText += result.Err;
+                } else {
+                    download(JSON.stringify(result.Ok, null, 2), args[1]);
+                }
+            } else {
+                output_el.innerText += "ERROR: unrecognized arguments for `tabledata`"
+            }
+        },
+        clear: () => {
+            output_el.innerText = "";
+            return "";
+        },
+    };
+    return commands;
 }
+
+
+function download(text, fileName) {
+    const blob = new Blob([text], { type: "text/plain" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
 
 function getCode() {
-  if (editor != undefined) code = editor.getValue();
+    return `pin 1, 2 = i[0..1];
+pin [14..17] = and, or, xor, not;
+
+table(i0, i1 -> and).fill(0) {
+    11 1
 }
 
-async function main() {
-  await wasm_bindgen("./rust_bg.wasm");
+table(i0, i1 -> or).fill(1) {
+    00 0
+}
 
-  setUpEditor(document.getElementById("container"));
+table(i0, i1 -> xor).count {
+    0
+    1
+    1
+    0
+}
 
-  document.getElementById("compile").onclick = () => {
-    getCode();
-    let result = rs_compile(code, galtype);
-    if (result.Err != null) {
-      document.getElementById("error").innerHTML = result.Err;
-    } else {
-      download(filename + ".jed", result.Ok);
-    }
-  };
-  document.getElementById("transpile").onclick = () => {
-    getCode();
-    let result = rs_transpile(code);
-    if (result.Err != null) {
-      document.getElementById("error").innerHTML = result.Err;
-    } else {
-      download(filename + ".PLD", result.Ok);
-    }
-  };
-  document.getElementById("tabledata").onclick = () => {
-    getCode();
-    let result = rs_tabledata(code);
-    if (result.Err != null) {
-      document.getElementById("error").innerHTML = result.Err;
-    } else {
-      download(filename + ".json", JSON.stringify(result.Ok));
-    }
-  };
+table(i0 -> not) {
+    01
+    10
+}
 
-  document.getElementById("downloadCode").onclick = () => {
-    getCode();
-    download(filename + ".ogal", code);
-  };
 
-  document.getElementById("galtype").onchange = () => {
-    galtype = document.getElementById("galtype").value;
-  };
+pin 23 = a;
+pin 3 = b;
+pin 2 = c;
 
-  document.getElementById("theme").onchange = () => {
-    theme = document.getElementById("theme").value;
-    getCode();
-    document.getElementById("container").innerHTML = null;
-    setUpEditor(document.getElementById("container"));
-  };
+a = (!b | (c));
+a.dff;`;
 }
